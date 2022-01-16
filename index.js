14deb41bd5fed12ad6b818a0cdb4a786d00d4ffb
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-01-15 18:42:15 -0700

    commit tree 407b4f46b809d7e6d4c357b86c8044e5f91a5952

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+const mondayWork = (activity="go to the office") => {
+    return `This Monday, I will ${activity}.`;
+};
+
+const wrapAdjective = (visFlair='*') => {
+    return (adj='special') => {
+        return `You are ${visFlair}${adj}${visFlair}!`;
+    };
+};
+
+const Calculator = {
+    add: (a, b) => {
+        return a + b;
+    },
+    subtract: (a, b) => {
+        return a - b;
+    },
+    multiply: (a, b) => {
+        return a * b;
+    },
+    divide: (a, b) => {
+        return a / b;
+    }
+}
+
+const actionApplyer = (int, array) => {
+    let start = int;
+
+    for (const elem of array) {
+        start = elem(start);
+    }
+
+    return start;
+}
